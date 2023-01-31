faf0f546b9058399edc4f970a08949be65291191
hennifant <henning.schwichow@gmx.de>
2023-01-31 10:49:10 +0100

    Added header, footer, basic setup

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="py-4 text-center bg-primary text-white ">
+      {" "}
+      &copy; 2023 hennifant
+    </div>
+  );
+};
+
+export default Footer;
